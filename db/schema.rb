6b8a3d958e1ebb3abc19abd42d5c759c04a3b97b
andrew <answer2005@gmail.com>
2014-09-14 23:58:22 +0300

    add heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140913230349) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assignments", force: true do |t|
     t.integer  "timeslot_id"
     t.integer  "boat_id"
